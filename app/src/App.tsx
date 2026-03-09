8dbc62b83ab6fde1a2995c8a5fa1de8f4fb8833d
MathieuSymphorien <72963501+MathieuSymphorien@users.noreply.github.com>
2026-03-09 17:44:39 +0100

    feat: add landingPage (en Y la page mais oklm)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,44 +1,10 @@
-import "./index.css";
-import type { ClientMessage } from "@ws-poc/shared/types";
-import { useReducer, useState } from "react";
-import { useWs } from "@/core/websocket/useWs";
-import { handleMessage } from "@/core/websocket/handlers";
-import { initialGameState } from "@/core/websocket/types";
+import LandingPage from "./pages/landingPage/landingPage";
 
 export function App() {
-  const [state, dispatch] = useReducer(handleMessage, initialGameState);
-  const { isReady, send } = useWs("ws://localhost:8080/ws", dispatch);
-  const [pseudo, setPseudo] = useState("");
-  const [joinCode, setJoinCode] = useState("");
-
-  function onCreateGame() {
-    if (isReady && pseudo) {
-      send(JSON.stringify({ event: "NEW_GAME", pseudo } as ClientMessage));
-    }
-  }
-
-  function onJoinGame() {
-    if (isReady && pseudo && joinCode) {
-      send(JSON.stringify({ event: "JOIN_GAME", pseudo, gameId: joinCode } as ClientMessage));
-    }
-  }
-
   return (
     <div>
-      <h1>WS POC</h1>
-      <input type="text" placeholder="Pseudo" value={pseudo} onChange={e => setPseudo(e.target.value)}/>
-      <button onClick={onCreateGame}>Create game</button>
-      <br />
-      <input type="text" placeholder="Code pour Rejoindre" value={joinCode} onChange={e => setJoinCode(e.target.value)}/>
-      <button onClick={onJoinGame}>Join Game</button>
-      {state.code && <p>{state.code}</p>}
-      <ul>
-        {state.players.map(player => (
-          <li key={player.uuid}>{player.pseudo}</li>
-        ))}
-      </ul>
+      <LandingPage />
     </div>
   );
 }
-
 export default App;
