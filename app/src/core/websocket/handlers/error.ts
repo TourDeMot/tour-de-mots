7b6ea56e2bdc3b491ec224ca7dd8d33d66d74fc2
MathieuSymphorien <72963501+MathieuSymphorien@users.noreply.github.com>
2026-03-12 14:14:31 +0100

    remode "error" from GameState and rename "code" in "gameId"

--- app/src/core/websocket/handlers/error.ts
+++ app/src/core/websocket/handlers/error.ts
@@ -2,5 +2,5 @@ import type { ErrorCode } from "@tour-de-mot/shared/types";
 import type { GameState } from "../types";
 
 export function handleError(state: GameState, code: ErrorCode): GameState {
-  return { ...state, error: code };
+  return { ...state};
 }
--- app/src/core/websocket/handlers/newGame.ts
+++ app/src/core/websocket/handlers/newGame.ts
@@ -2,5 +2,5 @@ import type { Player } from "@tour-de-mot/shared/types";
 import type { GameState } from "../types";
 
 export function handleNewGameOk(state: GameState, data: { gameId: string; players: Player[] }): GameState {
-  return { ...state, code: data.gameId, players: data.players };
+  return { ...state, gameId: data.gameId, players: data.players };
 }
