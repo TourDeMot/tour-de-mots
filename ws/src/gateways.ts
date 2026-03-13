b2693bfb80330caa20d660a6c6e5d462dce7d7e1
Pierre Teodoresco <p.teodoresco@gmail.com>
2026-03-13 15:15:13 +0100

    use generic typing and event payload map

--- ws/src/gateways.ts
+++ ws/src/gateways.ts
@@ -2,6 +2,8 @@ import { messageRouter } from "./routers";
 import type { ClientMessage, Game, SocketData } from "@tour-de-mot/shared/types";
 import { MISSING_UUID, BAD_JSON } from "@tour-de-mot/shared/error";
 import type { ServerWebSocket } from "bun";
+import type { ClientMessage } from "./messages";
+
 
 export const messageGateway = (
   ws: ServerWebSocket<SocketData>,
@@ -14,7 +16,7 @@ export const messageGateway = (
 
   try {
     const message = JSON.parse(raw.toString()) as ClientMessage;
-    messageRouter(ws, message, games);
+    messageRouter(ws, games, message);
   } catch (_) {
     ws.send(JSON.stringify(BAD_JSON));
   }
